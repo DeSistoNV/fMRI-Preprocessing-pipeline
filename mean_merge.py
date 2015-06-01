d4c997b75d4a476a7af7eb929f71975f6cdc64a7
desistonv@g.cofc.edu <desistonv@g.cofc.edu>
2015-05-31 21:16:43 -0400

    changed_mean_to_first_and_last

--- mean_merge.py
+++ mean_merge.py
@@ -6,8 +6,15 @@ import nipype.interfaces.fsl as fsl          # fsl
 import nipype.pipeline.engine as pe          # pypeline engine
 import nipype.interfaces.io as nio			 # for data grabbing and sinking
 import nipype.interfaces.utility as util	 # for defining input node
+import nipype.interfaces.fsl.utils as fslutils
 
-
+def first_and_last(l):
+	o = []
+	for i in l:
+		o.append(i.pop())
+		o.append(i.pop(0))
+	print o	
+	return o 
 ##=====define the pipeline engine
 preProc = pe.Workflow(name='meanmerge')
 
@@ -18,17 +25,21 @@ inputnode = pe.Node(interface=util.IdentityInterface(fields=['run_list']),
 		     name = 'mmInputnode')
 
 
-##node 1: get uncorrected mean
-mean = pe.MapNode(interface = fsl.maths.MeanImage(),
-	                              name = 'mean',
-				      iterfield = 'in_file')
-				 
+
+##split the 4D volumes up into individual 3D files
+split_out = pe.MapNode(interface = fslutils.Split(dimension = 't'),
+		    name = 'split_out',
+		    iterfield = ['in_file']
+		    )
+		       				 
 
 ##node 2: make a merged copy of the uncorrected volumes
 merge = pe.Node(interface = fsl.Merge(dimension='t'),
 					          name = 'merge')
 					          
 ##======connections
-preProc.connect(inputnode, 'run_list', mean, 'in_file')
-preProc.connect(mean, 'out_file', merge, 'in_files');
 
+
+preProc.connect([( inputnode, split_out,  [ ( 'run_list'                   , 'in_file'  )]),
+			     ( split_out,     merge,  [ ( ('out_files', first_and_last) ,'in_files')])
+ 		    	])
