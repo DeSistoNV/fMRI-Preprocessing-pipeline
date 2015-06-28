19c531a23e959dc8c4f62b3ca7d5ea996c808d26
desistonv <desistonv@g.cofc.edu>
2015-06-28 00:30:12 -0400

    cleaned up feat tools

--- AutoFeat/feat_automation_utils.py
+++ AutoFeat/feat_automation_utils.py
@@ -11,9 +11,11 @@ inputs = dict()
 def get_nvols(file_path):
     return subprocess.check_output(['fslstats',data,'-w']).split()[-1]
 
+# load a database, subject wise
 def load_db(csv,subj):
     ret = pd.read_csv(csv)
     return ret[ret.subject == 'CO']
+
 # load in template fsf file
 def get_template():
     with open('template.fsf') as f:
@@ -26,25 +28,19 @@ def build_runs(df,inp):
     for i in df.index:
         fmri = dict()
         fmri['input'] = df.run_data_file[i]
-
         fmri['outputdir'] = '{}/{}'.format(inp['out_dir'],df.runID[i]) 
         fmri['tr'] = df.TR[i]
-#         fmri['npts'] = get_nvols(fmri['input']) # n volumes
-        fmri['npts'] = df.nvols[i]
+        fmri['npts'] = df.nvols[i] # if we need to check : # get_nvols(fmri['input']) # n volumes
         fmri['multiple'] =  1
         fmri['mc'] = 1 if inp['Motion Correction'] else 0 # motion correction BOOL
         fmri['regunwarp_yn'] = 1 if inp['B0 Unwarping'] else 0 # B0 fieldmap unwarping?
         fmri['bet_yn'] = 1 if inp['Brain Extraction'] else 0 # BET brain extraction BOOL
         fmri['smooth'] = inp['Spatial Smooth'] # Spatial smoothing FWHM (mm)
         fmri['reginitial_highres_dof'] = inp['BBR DOF'] # Degrees of Freedom for registration to initial structural
-
         fmri['unwarp_dir'] = inp['Unwarp Direction'] # NO QOUTES # Unwarp direction
-
         fmri['reghighres_yn'] =  1 if inp['Register to Structural'] else 0 # Registration to main structural BOOL
 
         files = dict()
-
-
         files['feat_files(1)'] =  df.run_path[i] + '/' + df.run_data_file[i] + '/'
         files['feat_files(1)'] += [f for f in listdir(files['feat_files(1)']) if '.nii' in f][0]
         files['unwarp_files(1)'] =  df.field_map_phase[i]
@@ -55,6 +51,7 @@ def build_runs(df,inp):
         r.append((fmri,files))
     return r
 
+# replace inputs in the template fsf
 def build_fsf(fmri,files,cmds):
 
     ignores = ['ncopeinputs','filtering_yn','temphp_yn','templp_yn','ndelete','prewhiten_yn',
@@ -64,24 +61,25 @@ def build_fsf(fmri,files,cmds):
     cmds = [c for c in cmds if not any(rem in c for rem in remove)]
     for key in fmri:
         for i,cmd in enumerate(cmds):
-            cmd_splt = cmd.replace('(',',').replace(')',',').split(',')[1]
+            cmd_splt = cmd.replace('(',',').replace(')',',').split(',')[1] # grabbing the param out of the line
             if key in cmd and cmd_splt not in ignores:
                 cmds[i] = 'set fmri({}) {}'.format(key,fmri[key])
     for key in files:
         cmds.append('set {} "{}"'.format(key,files[key]))
     return cmds
 
+# save all the fsf files and return list of their paths
 def save_fsf(out,fsfs):
-    locations = []
+    p = []
     for i,f in enumerate(fsfs):
         location = '{}/{}.fsf'.format(out,i)
         with open(location,'w') as fi:
             for cmd in f:
                 fi.write(cmd + '\n')
-        locations.append(location)
-    return locations
+        p.append(location)
+    return p
 
-
+# runs feat on all the fsfs
 def run_feat_all(fs):
     for i, fsf in enumerate(fs):
         print 'run {}/{} running'.format(i+1,len(fs))
@@ -91,7 +89,7 @@ def run_feat_all(fs):
 
 
 
-#### 
+#### TESTING VARS #####
 # df['field_map_mag'] =  '/home/nick/Desktop/1_015_gre_field_mapping_1x1x2_20111013/naselaris_ca_20111013_001_015_gre_field_mapping_1x1x2.nii.gz'
 # df['field_map_phase'] ='/home/nick/Desktop/1_016_gre_field_mapping_1x1x2_20111013/naselaris_ca_20111013_001_016_gre_field_mapping_1x1x2.nii'
 # df['anatomical']= '/home/nick/Desktop/CO_s1000_T1.nii'
