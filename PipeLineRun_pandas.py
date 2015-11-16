9cd80db9a4f7929ac30fdb603f0e00820a722321
desistonv@g.cofc.edu <desistonv@g.cofc.edu>
2015-11-16 14:25:02 -0500

    fugue 1.1.1

--- PipeLineRun_pandas.py
+++ PipeLineRun_pandas.py
@@ -14,17 +14,19 @@ def pipe_it(subject, vox_res, fnirt, experiment):
     CHECK_SOCKET = True
 
     pipeline_data_params = dict()  # Select only...
-    pipeline_data_params['subject'] = subject
-    pipeline_data_params['experiment'] = experiment
-    pipeline_data_params['experiment'] = '3T.v.7T'
+    pipeline_data_params['subject'] = [subject]
+    pipeline_data_params['experiment'] = [experiment]
     pipeline_data_params['run_list'] = []
     pipeline_data_params['sess_list'] = []
     pipeline_data_params['vox_res'] = []
+    pipeline_data_params['run_type'] = []
     pipeline_data_params['crop_this'] = ['med', 'med', 'min']  # Almost never used
     if experiment == 'imagery.rf':
         pipeline_data_params['db'] = '/musc.repo/Data/nickdesisto/Imagery_preproc/Imagery_DB.csv'
     elif experiment == '3T.v.7T':
         pipeline_data_params['db'] = '/musc.repo/Data/nickdesisto/3T.v.7T/Databii/res{}.csv'.format(vox_res)
+    elif experiment == 'lotusHill.7T.em9K.1.5iso' or 'lotusHill.7T.em9K.1.5iso_first_try':
+        pipeline_data_params['db'] = '/mnt/em9k.csv'
     else:
         sys.exit('invalid experiment passed')
 
@@ -32,9 +34,10 @@ def pipe_it(subject, vox_res, fnirt, experiment):
         subj = pipeline_data_params['subject'],
         expID = pipeline_data_params['experiment'],
         runList = pipeline_data_params['run_list'],
-        sessList=pipeline_data_params['sess_list'],
+        sess_list=pipeline_data_params['sess_list'],
         cropRule = pipeline_data_params['crop_this'],
         db = pipeline_data_params['db'],
+        run_type = pipeline_data_params['run_type']
         # vox_res = pipeline_data_params['vox_res'],
         # do_fugue = pipeline_data_params['do_fugue']
         )
@@ -49,21 +52,24 @@ def pipe_it(subject, vox_res, fnirt, experiment):
             base = '/home/nickdesisto/ichi/musc.repo/Data/nickdesisto/Imagery_preproc/testing/no_fnirt/{}_{}'
             fsl_preproc_params['results_base_dir'] = base.format(subject, vox_res, fnirt_text)
         elif host_name == 'Ichi':
-            results = '/mnt/3T.v.7T_pipe/testing/changing_merge/{}/{}_{}'
-            fsl_preproc_params['results_base_dir'] = results.format(subject, vox_res, fnirt_text)
+            results = '/mnt/initial_em9k_pipe/{}_{}_first_try/'
+            fsl_preproc_params['results_base_dir'] = results.format(subject, fnirt_text)
             base = '/mnt/nipype_intermediate_dump/{}/{}_{}_{}'
             fsl_preproc_params['basedir'] = base.format(experiment, subject, vox_res, fnirt_text)
         else:
-            sys.exit('What computer are you on?')
+         results = '/home2/initial_3t7t_pipe/{}_{}_first_try/'
+         fsl_preproc_params['results_base_dir'] = results.format(subject, fnirt_text)
+         base = '/home2/nipype_intermediate_dump/{}/{}_{}_{}'
+    	 fsl_preproc_params['basedir'] = base.format(experiment, subject, vox_res, fnirt_text)
     else: 
         fsl_preproc_params['basedir'] = '/home/nick/nipype_intermediate_dump/'
         fsl_preproc_params['results_base_dir'] = '/home/nick/results/'
 
     # default_params['ref_vol_runList'] = [0]       #grab only the first run for aligning all the other runs to
-    fsl_preproc_params['results_container'] = 'Imagery_RF_test' if experiment == 'imagery.rf' else '3T.v.7T'
+    fsl_preproc_params['results_container'] = experiment
     fsl_preproc_params['convert_dicoms'] = False
     fsl_preproc_params['t_size'] = 10000  # just go with it (max possible size).
-    fsl_preproc_params['bet_frac'] = 0.08  # BRAIN EXTRACTION TOOL THRESHOLD
+    fsl_preproc_params['bet_frac'] = 0.3  # BRAIN EXTRACTION TOOL THRESHOLD
     # motion correction
     fsl_preproc_params['ref_run'] = []  # reference volume all others will be referenced
     fsl_preproc_params['moco_only'] = False  # if true, all FLIRT AND FNIRT is ignored
@@ -73,7 +79,7 @@ def pipe_it(subject, vox_res, fnirt, experiment):
     fsl_preproc_params['dof_FLIRT'] = 6  # degrees of freedom (MAX : 12)
     fsl_preproc_params['rigid2D_FLIRT'] = False  # if true, restrict linear reg to rigid body transformations and ignore dof
     # nonlinear registration
-    fsl_preproc_params['do_FNIRT'] = fnirt
+    fsl_preproc_params['do_FNIRT'] = True
     fsl_preproc_params['searchr_x'] = []  # params for linear reg exposed (select the angular range over which the initial optimisation search stage is performed.)
     fsl_preproc_params['searchr_y'] = []  # find out what these guys do
     fsl_preproc_params['searchr_z'] = []
@@ -82,6 +88,13 @@ def pipe_it(subject, vox_res, fnirt, experiment):
     fsl_preproc_params['FNIRT_warpres'] = [(5, 5, 5)]  # Resolution of the warping function. Like, how fine is the warping. Can specify different level for each iteration. *Question*: Why is this list shorter than the one above?
     # run settings
     fsl_preproc_params['nProc'] = 1 # number of CPUS
+
+    fsl_preproc_params['delta_TE'] = 1.02
+    fsl_preproc_params[ 'mag_frac'] = .5
+    fsl_preproc_params['do_fugue'] = True
+
+
+
 
     check_params = False
     if check_params:
@@ -93,10 +106,11 @@ def pipe_it(subject, vox_res, fnirt, experiment):
         setattr(main_inputnode.inputs, key, fsl_preproc_params[key])
     # => construct the workflow
     pp = create_fsl_preproc_workflow(main_inputnode)
+    pp.write_graph()
     # and, for reasons I can't remember, establish the base directory as an attribute of the workflow
     pp.base_dir = fsl_preproc_params['basedir']
     # print inputs after being processed by mriDbObjDefs for sanity check
-    print main_inputnode.inputs
+    # print main_inputnode.inputs
     # run the pipeline
     if fsl_preproc_params['nProc'] > 1:
         pp.run(plugin='MultiProc', plugin_args={'n_procs': fsl_preproc_params['nProc']})
@@ -104,9 +118,9 @@ def pipe_it(subject, vox_res, fnirt, experiment):
         pp.run()
 
     # Appending Working Volume and Brain Mask file paths to a new CSV saved in results_base_dir
-    add_mask_vols(pp.get_node('inputnode'), fsl_preproc_params, pipeline_data_params['db'])
+    # add_mask_vols(pp.get_node('inputnode'), fsl_preproc_params, pipeline_data_params['db'])
     # outputs a text file of parameters used
-    params_txt(fsl_preproc_params)
+    #params_txt(fsl_preproc_params)
     # option to append a parameters csv to keep track when doing iterative optimization
     param_csv = False
 
@@ -119,15 +133,24 @@ def pipe_it(subject, vox_res, fnirt, experiment):
 
 
 def main():
-    # current experiments : imagery.rf , 3T.v.7T
+    # current experiments : imagery.rf , 3T.v.7T, lotusHill.7T.em9K.1.5iso
     # pipe_it(subject (str), vox_res (str), fnirt (bool), experiment(str) ):
     host_name = socket.gethostname()
     if host_name == 'Ichi':
         #      (Subject, VoxelRes, FNIRT?, expID)
-        pipe_it('s1000', '3', False, '3T.v.7T')
+       # pipe_it('CO', '1.5', False, 'lotusHill.7T.em9K.1.5iso')
+       # pipe_it('DM', '1.5', True, 'lotusHill.7T.em9K.1.5iso')
+       # pipe_it('MY', '1.5', False, 'lotusHill.7T.em9K.1.5iso')
+       # pipe_it('PHIL', '1.5', True, 'lotusHill.7T.em9K.1.5iso')
+       #pipe_it('TN', '1.5', False, 'lotusHill.7T.em9K.1.5iso')
+       pipe_it('TN','1.6',True,'imagery.rf')
+       #pipe_it('s1000','3',False, '3T.v.7T')
+       
 
     if host_name == 'san':
         pass
+
+    pipe_it('s1000','3',False, '3T.v.7T')
 
 
 
